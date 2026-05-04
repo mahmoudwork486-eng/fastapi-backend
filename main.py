db3d9d7e73f4680c0b3d1bc0fa8e0ab3ecb68dd1
Mahmoud <mahmoudwork486@gmail.com>
2026-05-04 22:22:26 +0000

    fastapi backend

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API is working"}
+
+@app.post("/predict")
+def predict(data: dict):
+    return {"result": "ok"}
