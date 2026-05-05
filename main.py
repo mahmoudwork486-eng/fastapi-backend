6aff952c8c94b70ed0357cb796b8c7969bb46bde
mahmoudwork486-eng <mahmoudwork486@gmail.com>
2026-05-05 03:44:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from fastapi import FastAPI
-import pickle
+import joblib
 
-app = FastAPI()
-
-model = pickle.load(open("spam_model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
-
-@app.get("/")
-def home():
-    return {"message": "API is working"}
-
-@app.post("/predict")
-def predict(data: dict):
-    text = data["text"]
-    text_vector = vectorizer.transform([text])
-    prediction = model.predict(text_vector)[0]
-    return {"result": int(prediction)}
+model = joblib.load("spam_model.pkl")
