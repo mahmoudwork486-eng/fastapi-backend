81320457f53b250c68702d4ba7a939391625b4e4
mahmoudwork486-eng <mahmoudwork486@gmail.com>
2026-05-05 04:12:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
+with open("main.py", "w") as f:
+    f.write("""
 from fastapi import FastAPI
-import joblib
 
 app = FastAPI()
-
-# تحميل الموديل
 model = joblib.load("spam_model.pkl")
-
 @app.get("/")
 def home():
     return {"message": "API is working"}
 
 @app.post("/predict")
 def predict(data: dict):
-    text = data["text"]
-    prediction = model.predict([text])[0]
-    return {"result": int(prediction)}
+    return {"result": "ok"}
+""")
